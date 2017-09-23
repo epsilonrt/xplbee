bf0664548f1ef474e47765b3dab8e38d644bbca2
epsilonrt <epsilonrt@gmail.com>
2017-09-23 23:03:14 +0200

    LED Arduino incorecte
    La led est connecté sur PB7 et non PB0.

--- src/arduino/xplbee/variants/standard/pins_arduino.h
+++ src/arduino/xplbee/variants/standard/pins_arduino.h
@@ -4,36 +4,36 @@
 #include <avr/pgmspace.h>
 /*
 
-                 MIGHTYCORE STANDARD PINOUT
-         ATmega8535, ATmega16, ATmega32, ATmega164, 
-              ATmega324, ATmega644, ATmega1284
-   	 
-                         +---\/---+
-         LED (D 0) PB0  1|        |40  PA0 (A 0 / D24)
-             (D 1) PB1  2|        |39  PA1 (A 1 / D25)
-        INT2 (D 2) PB2  3|        |38  PA2 (A 2 / D26)
-         PWM (D 3) PB3  4|        |37  PA3 (A 3 / D27)
-    PWM* /SS (D 4) PB4  5|        |36  PA4 (A 4 / D28)
-        MOSI (D 5) PB5  6|        |35  PA5 (A 5 / D29)
- PWM** /MISO (D 6) PB6  7|        |34  PA6 (A 6 / D30)
-  PWM** /SCK (D 7) PB7  8|        |33  PA7 (A 7 / D31)
-                   RST  9|        |32  AREF
-                   VCC 10|        |31  GND 
-                   GND 11|        |30  AVCC
-                 XTAL2 12|        |29  PC7 (D 23)
-                 XTAL1 13|        |28  PC6 (D 22)
-        RX0 (D 8)  PD0 14|        |27  PC5 (D 21) TDI
-        TX0 (D 9)  PD1 15|        |26  PC4 (D 20) TDO
- (RX1)/INT0 (D 10) PD2 16|        |25  PC3 (D 19) TMS
- (TX1)/INT1 (D 11) PD3 17|        |24  PC2 (D 18) TCK
-        PWM (D 12) PD4 18|        |23  PC1 (D 17) SDA
-        PWM (D 13) PD5 19|        |22  PC0 (D 16) SCL
-       PWM* (D 14) PD6 20|        |21  PD7 (D 15) PWM
-                         +--------+
-       PWM: ATmega8535/16/32/164/324/644/1284
-       PWM*: ATmega164/324/644/1284
-       PWM**: ATmega1284
-       PCINT ONLY ON ATmega164/324/644/1284
+                       MIGHTYCORE STANDARD PINOUT
+               ATmega8535, ATmega16, ATmega32, ATmega164, 
+                    ATmega324, ATmega644, ATmega1284
+           
+                               +---\/---+
+                   (D 0) PB0  1|        |40  PA0 (A 0 / D24)
+                   (D 1) PB1  2|        |39  PA1 (A 1 / D25)
+              INT2 (D 2) PB2  3|        |38  PA2 (A 2 / D26)
+               PWM (D 3) PB3  4|        |37  PA3 (A 3 / D27)
+          PWM* /SS (D 4) PB4  5|        |36  PA4 (A 4 / D28)
+              MOSI (D 5) PB5  6|        |35  PA5 (A 5 / D29)
+       PWM** /MISO (D 6) PB6  7|        |34  PA6 (A 6 / D30)
+    PWM** LED /SCK (D 7) PB7  8|        |33  PA7 (A 7 / D31)
+                         RST  9|        |32  AREF
+                         VCC 10|        |31  GND 
+                         GND 11|        |30  AVCC
+                       XTAL2 12|        |29  PC7 (D 23)
+                       XTAL1 13|        |28  PC6 (D 22)
+              RX0 (D 8)  PD0 14|        |27  PC5 (D 21) TDI
+              TX0 (D 9)  PD1 15|        |26  PC4 (D 20) TDO
+       (RX1)/INT0 (D 10) PD2 16|        |25  PC3 (D 19) TMS
+       (TX1)/INT1 (D 11) PD3 17|        |24  PC2 (D 18) TCK
+              PWM (D 12) PD4 18|        |23  PC1 (D 17) SDA
+              PWM (D 13) PD5 19|        |22  PC0 (D 16) SCL
+             PWM* (D 14) PD6 20|        |21  PD7 (D 15) PWM
+                               +--------+
+             PWM: ATmega8535/16/32/164/324/644/1284
+             PWM*: ATmega164/324/644/1284
+             PWM**: ATmega1284
+             PCINT ONLY ON ATmega164/324/644/1284
    
 PCINT15-8: D7-0    : bit 1
 PCINT31-24: D15-8  : bit 3
@@ -62,8 +62,8 @@ defined(__AVR_ATmega644P__)
 #endif
 
 
-static const uint8_t LED = 0;
-#define LED_BUILTIN 0
+static const uint8_t LED = 7;
+#define LED_BUILTIN 7
 
 static const uint8_t SS   = 4;
 static const uint8_t MOSI = 5;
